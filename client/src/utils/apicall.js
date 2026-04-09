ce0c9f538a88a6306aa5438e294ad7c09b597ebf
Ishu saini <ishuwork8394@gmail.com>
2026-04-09 14:11:10 +0530

    Update apicall.js

--- client/src/utils/apicall.js
+++ client/src/utils/apicall.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://crm-project-9pob.onrender.com",
+    baseURL: "https://crm-project-9pob.onrender.com/api",
     withCredentials: true
 })
 
